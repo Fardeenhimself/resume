1323b0fbae09682b94226c32a4db2d7d28f6669c
Fardeenhimself <110120932+Fardeenhimself@users.noreply.github.com>
2023-09-07 22:35:53 +0600

    Files have been uploaded

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        test: ['Roboto Slab']
+      }
+    },
+  },
+  plugins: [],
+}
+
